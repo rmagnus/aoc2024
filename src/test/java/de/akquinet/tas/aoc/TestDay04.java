ef94c74c55e0bc8b7196831ebe165c713f44af67
Robert Magnus <robert.magnus@akquinet.de>
2024-12-05 17:39:19 +0100

    Code Review

--- src/test/java/de/akquinet/tas/aoc/TestDay04.java
+++ src/test/java/de/akquinet/tas/aoc/TestDay04.java
@@ -14,8 +14,6 @@ class TestDay04 {
 
     private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-    private char[] xmas = { 'X', 'M', 'A', 'S' };
-
     @Test
     void getPart1XMASSearch() throws IOException {
         LOG.info("getPart1XMASSearch()");
@@ -36,69 +34,59 @@ class TestDay04 {
 
         int result = 0;
 
-        // Für die "gerade" Sachen brauchen wir keine extra Inside-Be
-
         // vorwärts, jede Zeile scannen
         for (int i = 0; i < heigth; i++) {
             result = result + checkXMAS(array, i, 0, 0, 1,
-                    (arry, x, y) ->  (y == width),
-                    (arry, x, y) ->  (y < width)
+                    (arry, x, y) ->  (y == width)
                     );
         }
 
         // rückwärts, jede Zeile scannen
         for (int i = 0; i < heigth; i++) {
             result = result + checkXMAS(array, i, width - 1, 0, -1,
-                    (arry, x, y) ->  (y == 0),
-                    (arry, x, y) ->  (y >= 0)
+                    (arry, x, y) ->  (y == 0)
                     );
         }
 
         // runter, jede Spalte scannen
         for (int i = 0; i < width; i++) {
             result = result + checkXMAS(array, 0, i , 1, 0,
-                    (arry, x, y) ->  (x == heigth),
-                    (arry, x, y) ->  ((x < heigth) && (y < width))
+                    (arry, x, y) ->  (x == heigth)
                     );
         }
 
         // hoch, jede Spalte scannen
         for (int i = 0; i < width; i++) {
             result = result + checkXMAS(array, heigth - 1, i, -1, 0,
-                    (arry, x, y) ->  (x == 0),
-                    (arry, x, y) ->  ((x >= 0) && (y >= 0))
+                    (arry, x, y) ->  (x == 0)
                     );
         }
 
         // vorwärts und runter, jede Diagonale scannen
         for (int i = 0 - heigth; i < width; i++) {
             result = result + checkXMAS(array, 0, i, 1, 1,
-                    (arry, x, y) ->  ((y == width) || (x == heigth)),
-                    (arry, x, y) ->  ((x < heigth) && (x >= 0) && (y >= 0) && (y < heigth))
+                    (arry, x, y) ->  (y == width)
                     );
         }
 
         // vorwärts und hoch, jede Diagonale scannen
         for (int i = 0 - heigth; i < width; i++) {
             result = result + checkXMAS(array, heigth - 1, i, -1, 1,
-                    (arry, x, y) ->  ((x == 0) || (y == width)),
-                    (arry, x, y) ->  ((x < heigth) && (x >= 0) && (y >= 0) && (y < heigth))
+                    (arry, x, y) ->  (y == width)
                     );
         }
 
         // rückwärts und runter, jede Diagonale scannen
         for (int i = 0; i < (width + heigth); i++) {
             result = result + checkXMAS(array, 0, i, 1, -1,
-                    (arry, x, y) ->  (x == heigth),
-                    (arry, x, y) ->  ((x < heigth) && (x >= 0) && (y >= 0) && (y < heigth))
+                    (arry, x, y) ->  (x == heigth)
                     );
         }
 
         // rückwärts und hoch, jede Diagonale scannen
         for (int i = 0; i < (width + heigth); i++) {
             result = result + checkXMAS(array, heigth - 1, i, -1, -1,
-                    (arry, x, y) ->  (x == 0),
-                    (arry, x, y) ->  ((x < heigth) && (x >= 0) && (y >= 0) && (y < heigth))
+                    (arry, x, y) ->  (x == 0)
                     );
         }
 
@@ -109,18 +97,14 @@ class TestDay04 {
 
     }
 
-    private int checkXMAS(char[][] array, int x, int y, int dx, int dy, BorderCondition borderCondition,
-            InsideCondition ic) {
-        if (borderCondition.isReached(array, x, y)) {
-            return 0;
-        }
-
+    private int checkXMAS(char[][] array, int x, int y, int dx, int dy, BorderCondition borderCondition)
+    {
         int count = 0;
 
         while (!(borderCondition.isReached(array, x, y))) {
             // liegt mein Suchbereich im Array?
-            if (ic.isInside(array, x, y) && ic.isInside(array, x + 3 * dx, y + 3 * dy)) {
-                // wenn die 4 Zeichen in Suchrichtung 'xmas' sind
+            if (inside(array, x, y) && inside(array, x + 3 * dx, y + 3 * dy)) {
+                // wenn die 4 Zeichen in Suchrichtung 'XMAS' sind
                 String str = "" + array[x][y] + array[x + 1 * dx][y + 1 * dy] + array[x + 2 * dx][y + 2 * dy]
                         + array[x + 3 * dx][y + 3 * dy];
                 if ("XMAS".equals(str)) {
@@ -129,22 +113,23 @@ class TestDay04 {
                     y = y + 2 * dy;
                 }
             }
-                x = x + dx;
-                y = y + dy;
+            x = x + dx;
+            y = y + dy;
         }
 
         return count;
 
     }
 
-    @FunctionalInterface
-    interface BorderCondition {
-        boolean isReached(char[][] matrix, int currentRow, int currentCol);
+    private boolean inside(char[][] array, int x, int y) {
+        int heigth = array.length;
+        int width = array[0].length;
+        return (x < heigth) && (x >= 0) && (y >= 0) && (y < width);
     }
 
     @FunctionalInterface
-    interface InsideCondition {
-        boolean isInside(char[][] matrix, int currentRow, int currentCol);
+    interface BorderCondition {
+        boolean isReached(char[][] matrix, int currentRow, int currentCol);
     }
 
 }
